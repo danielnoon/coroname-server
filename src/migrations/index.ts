5e6783f41f18147b9c9d4896b962c8ee00b10ec2
Daniel Noon <supercookies@outlook.com>
2020-05-08 19:02:16 -0500

    fix issue with migration application

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -6,14 +6,18 @@ const migrations: Migration[] = [addSupervote];
 
 export async function runMigrations() {
   let lastUpdate = -1;
+  const updates = await Update.find({});
+
   while (
-    !(await Update.exists({ version: lastUpdate + 1 })) ||
-    lastUpdate + 1 <= migrations.length
+    !updates.find(
+      (update) => update.version === migrations[lastUpdate + 1].version
+    ) &&
+    lastUpdate + 1 < migrations.length
   ) {
     lastUpdate++;
   }
 
-  for (let i = lastUpdate; i > 0; i--) {
+  for (let i = lastUpdate; i >= 0; i--) {
     await migrations[i].applyUpdate();
     const update = new Update({ version: migrations[i].version });
     await update.save();
