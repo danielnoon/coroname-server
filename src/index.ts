1049b83bedf8c5680736cafb4550da55e45aced8
Daniel Noon <supercookies@outlook.com>
2020-05-09 19:00:45 -0500

    oop

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,37 @@
-import express, { Request, Response } from 'express';
-import mongoose from 'mongoose';
-import auth from './controllers/auth';
-import anime from './controllers/anime';
-import admin from './controllers/admin';
-import users from './controllers/users';
-import cors from 'cors';
-import bp from 'body-parser';
-import { HttpError } from './http-error';
-import { error } from './models/error';
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import auth from "./controllers/auth";
+import anime from "./controllers/anime";
+import admin from "./controllers/admin";
+import users from "./controllers/users";
+import cors from "cors";
+import bp from "body-parser";
+import { HttpError } from "./http-error";
+import { error } from "./models/error";
+import { runMigrations } from "./migrations";
 
 const app = express();
 
-app.use(cors({methods: ['POST', 'GET', 'OPTIONS', 'DELETE', 'PUT', 'PATCH']}));
+app.use(
+  cors({ methods: ["POST", "GET", "OPTIONS", "DELETE", "PUT", "PATCH"] })
+);
 app.use(bp.json());
 
-mongoose.connect(process.env.DB_URI || 'mongodb://localhost:27017/coroname', {
-  useNewUrlParser: true, useUnifiedTopology: true
+mongoose.connect(process.env.DB_URI || "mongodb://localhost:27017/coroname", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-app.get('/', (req, res) => {
-  res.redirect('https://coroname.net');
+app.get("/", (req, res) => {
+  res.redirect("https://coroname.net");
 });
 
-app.use('/auth', auth);
-app.use('/anime', anime);
-app.use('/admin', admin);
-app.use('/users', users);
+app.use("/auth", auth);
+app.use("/anime", anime);
+app.use("/admin", admin);
+app.use("/users", users);
 
-app.use((err: HttpError, req: Request<any>, res: Response<any>, next: () => void) => {
-  res.status(err.code || 500).send(error(err.message) || "Internal server error.");
-  next();
-});
-
-app.listen(process.env.PORT || 3000, () => console.log("Listening!"));
+async () => {
+  await runMigrations();
+  app.listen(process.env.PORT || 3000, () => console.log("Listening!"));
+};
--- src/thunk.ts
+++ src/thunk.ts
@@ -1,5 +1,14 @@
 import { Request, Response, NextFunction } from "express";
+import { error } from "./models/error";
 
-const wrap = (fn: (req: Request, res: Response, next: NextFunction) => Promise<any>) => (req: Request, res: Response, next: NextFunction) => fn(req, res, next).catch(next);
+const wrap = (
+  fn: (req: Request, res: Response, next: NextFunction) => Promise<any>
+) => (req: Request, res: Response, next: NextFunction) =>
+  fn(req, res, next).catch((err) => {
+    res
+      .status(err.code || 500)
+      .send(error(err.message) || "Internal server error.");
+    next();
+  });
 
 export default wrap;
