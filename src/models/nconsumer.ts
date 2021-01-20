bc99f2c4f1758e5faf580c670056d102a5c9d290
Daniel Noon <supercookies@outlook.com>
2021-01-20 17:37:47 -0600

    fix consumer issue

--- src/models/nconsumer.ts
+++ src/models/nconsumer.ts
@@ -3,11 +3,13 @@ import mongoose, { Schema, Document } from "mongoose";
 export interface INConsumer extends Document {
   token: string;
   user: string;
+  subscribed: boolean;
 }
 
 const NConsumerSchema = new Schema({
   token: String,
   user: Schema.Types.ObjectId,
+  subscribed: Boolean,
 });
 
 export const NConsumer = mongoose.model<INConsumer>(
